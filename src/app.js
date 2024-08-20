8a009dd6769c2d52c7176d0f8c877bf655abd5c3
Dahara <github@dahara.de>
2024-08-20 07:34:00 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,15 +1,22 @@
-import WebSocket from 'ws';
-import {createRequire} from 'module';
+// Import the WebSocket module for real-time communication
+import WebSocket from "ws";
 
+// Import the createRequire function to enable the use of CommonJS modules in an ES module environment
+import { createRequire } from "module";
+
+// Create a require function using the current module's URL
 const require = createRequire(import.meta.url);
-const irsdk = require('node-irsdk-2023');
 
+// Require the node-irsdk-2023 package using the created require function
+const irsdk = require("node-irsdk-2023");
+
+// Export irsdk and WebSocket so they can be used in other modules
 export { irsdk, WebSocket };
 
-import Iracing from './classes/iracing.js';
-import Connection from './classes/connection.js';
+// Import the Iracing and Connection classes from their respective files
+import Iracing from "./classes/iracing.js";
+import Connection from "./classes/connection.js";
 
+// Create instances of Iracing and Connection classes
 export const iracing = new Iracing();
 export const connection = new Connection();
-
-//connection.connect();
