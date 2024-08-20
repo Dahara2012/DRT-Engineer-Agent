4221e2ac77b45dc3692f75d5a6c581e2178634a5
Dahara <github@dahara.de>
2024-08-20 07:08:47 +0200

    Jest install

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # DRT-Engineer-Agent
- DRT Race Engineer Tool
+
+DRT Race Engineer Tool
+
+## Dependencies
+
+Before installing check that you have git and node insatalled
+https://git-scm.com/downloads
+https://nodejs.org/en/download/prebuilt-installer
+
+## Installing
+
+Open a Terminal
+Go to the directory you want to install the agent in
+type: git clone
--- src/classes/connection.js
+++ src/classes/connection.js
@@ -1,120 +1,128 @@
 import { WebSocket, iracing } from "../app.js";
 
 export default class Connection {
-	constructor() {
-		this._connected = false;
-		this._isReconnecting = false;
-		this._shouldReconnect = true;
-	}
+  constructor() {
+    this._connected = false;
+    this._isReconnecting = false;
+    this._shouldReconnect = true;
+  }
 
-	get connected(){
-		return this._connected;
-	}
+  get connected() {
+    return this._connected;
+  }
 
-	get isReconnecting(){
-		return this._isReconnecting;
-	}
+  get isReconnecting() {
+    return this._isReconnecting;
+  }
 
-	get shouldReconnect(){
-		return this._shouldReconnect;
-	}
+  get shouldReconnect() {
+    return this._shouldReconnect;
+  }
 
-	set connected(bool){
-		this._connected = bool;
-	}
+  set connected(bool) {
+    this._connected = bool;
+  }
 
-	set isReconnecting(bool){
-		this._isReconnecting = bool;
-	}
+  set isReconnecting(bool) {
+    this._isReconnecting = bool;
+  }
 
-	set shouldReconnect(bool){
-		this._shouldReconnect = bool;
-	}
+  set shouldReconnect(bool) {
+    this._shouldReconnect = bool;
+  }
 
-	sendMessage({key = 'key', value = 'value'} = {}) {
-		if (this.connected === true){
-			const message = {key: key, value: value, driverid: iracing.driverId, teamId: iracing.teamId};
-			this.ws.send(this._stringifyJSON(message));
-			//console.log(`send ${key}`);
-		}else{
-			console.log(`tried to send ${key}, but Websocket not connected`);
-		}
-		
-	}
+  sendMessage({ key = "key", value = "value" } = {}) {
+    if (this.connected === true) {
+      const message = {
+        key: key,
+        value: value,
+        driverid: iracing.driverId,
+        teamId: iracing.teamId,
+      };
+      this.ws.send(this._stringifyJSON(message));
+      //console.log(`send ${key}`);
+    } else {
+      console.log(`tried to send ${key}, but Websocket not connected`);
+    }
+  }
 
-	connect() {
-		if(this.connected){ return; }
-		this.ws = new WebSocket('ws://127.0.0.1:13001');
+  connect() {
+    if (this.connected) {
+      return;
+    }
+    this.ws = new WebSocket("wss://127.0.0.1:13001", {
+      rejectUnauthorized: false, // Only for self-signed certificates in development
+    });
 
-		this.ws.onopen = () => {
-			this.connected = true;
-			this.sendMessage({key: "agentInit", value:""})
-			console.log('WebSocket connected');
-		}
+    this.ws.onopen = () => {
+      this.connected = true;
+      this.sendMessage({ key: "agentInit", value: "" });
+      console.log("WebSocket connected");
+    };
 
-		this.ws.onclose = (e) => {
-			this.connected = false;
-			console.log('WebSocket was closed. ' + e);
-			if (this.shouldReconnect) {
-				this._reconnect();
-			}
-		}
+    this.ws.onclose = (e) => {
+      this.connected = false;
+      console.log("WebSocket was closed. " + e);
+      if (this.shouldReconnect) {
+        this._reconnect();
+      }
+    };
 
-		this.ws.onerror = (error) => {
-			this.connected = false;
-			console.error('WebSocket encountered error: ' + error.message);
-			if (this.shouldReconnect) {
-				this._reconnect();
-			}
-		}
+    this.ws.onerror = (error) => {
+      this.connected = false;
+      console.error("WebSocket encountered error: " + error.message);
+      if (this.shouldReconnect) {
+        this._reconnect();
+      }
+    };
 
-		this.ws.onmessage = (e) => {
-			this.connected = true;
-			const message = this._parseJSON(e.data.toString());
+    this.ws.onmessage = (e) => {
+      this.connected = true;
+      const message = this._parseJSON(e.data.toString());
 
-			if (message.key == "Code"){
-				console.log(`Your Code is: ${message.value}`);
-			}
-		}
-	}
+      if (message.key == "Code") {
+        console.log(`Your Code is: ${message.value}`);
+      }
+    };
+  }
 
-	_reconnect() {
-		if (this.isReconnecting) {
-			return;
-		}
+  _reconnect() {
+    if (this.isReconnecting) {
+      return;
+    }
 
-		this.isReconnecting = true;
+    this.isReconnecting = true;
 
-		let minDelay = 15000; // 15 seconds
-		let maxDelay = 30000; // 30 seconds
-		let delay = Math.random() * (maxDelay - minDelay) + minDelay;
+    let minDelay = 15000; // 15 seconds
+    let maxDelay = 30000; // 30 seconds
+    let delay = Math.random() * (maxDelay - minDelay) + minDelay;
 
-		setTimeout(() => {
-			this.connect();
-			this.isReconnecting = false;
-		}, delay);
-	}
+    setTimeout(() => {
+      this.connect();
+      this.isReconnecting = false;
+    }, delay);
+  }
 
-	_parseJSON(string) {
-		return JSON.parse(string);
-	}
+  _parseJSON(string) {
+    return JSON.parse(string);
+  }
 
-	_stringifyJSON(object) {
-		return JSON.stringify(object)
-	}
+  _stringifyJSON(object) {
+    return JSON.stringify(object);
+  }
 
-	_compareVersions(version1, version2) {
-		const v1Parts = version1.split('.').map(Number);
-		const v2Parts = version2.split('.').map(Number);
+  _compareVersions(version1, version2) {
+    const v1Parts = version1.split(".").map(Number);
+    const v2Parts = version2.split(".").map(Number);
 
-		for (let i = 0; i < Math.max(v1Parts.length, v2Parts.length); i++) {
-			const v1 = v1Parts[i] || 0;
-			const v2 = v2Parts[i] || 0;
+    for (let i = 0; i < Math.max(v1Parts.length, v2Parts.length); i++) {
+      const v1 = v1Parts[i] || 0;
+      const v2 = v2Parts[i] || 0;
 
-			if (v1 > v2) return 1;
-			if (v1 < v2) return -1;
-		}
+      if (v1 > v2) return 1;
+      if (v1 < v2) return -1;
+    }
 
-		return 0;
-	}
+    return 0;
+  }
 }
